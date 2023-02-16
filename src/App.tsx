e0636de37fc82820b4504e60a6df0b3459711fda
HashRei <hashrei@protonmail.com>
2023-02-16 12:38:52 +0100

    Fix an ability to display all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { TodoForm } from './components/TodoForm';
-import { TodoList } from './components/TodoList';
-import { TodoResults } from './components/TodoResults';
-import { TodosProvider } from './TodosContext';
+import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { TodoResults } from "./components/TodoResults";
+import { TodosProvider } from "./TodosContext";
 
-import todosTemplate from './data/todos.json';
-import './App.scss';
+import todosTemplate from "./data/todos.json";
+import "./App.scss";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    setTodos(todosTemplate)
+  },[]);
+
   return (
     <div className="root">
-      <TodosProvider value={{ todos }}>
+      <TodosProvider value={{ todos, setTodos }}>
         <TodoList />
         <TodoResults />
         <TodoForm />
       </TodosProvider>
     </div>
   );
-}
+};
 
 export default App;
