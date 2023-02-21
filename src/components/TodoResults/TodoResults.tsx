69d4f64ccbb49f95f16105ba5343979f57a40b61
HashRei <hashrei@protonmail.com>
2023-02-21 14:57:10 +0100

    Fix an ability to count completed tasks

--- src/components/TodoResults/TodoResults.tsx
+++ src/components/TodoResults/TodoResults.tsx
@@ -1,14 +1,17 @@
-import './TodoResults.scss';
+import { useContext } from "react";
+import { todosContext } from "../../TodosContext";
+import "./TodoResults.scss";
 
 export const TodoResults = () => {
+  const { todos } = useContext(todosContext);
   const calculateChecked = () => {
     // Fix an ability to calculate completed tasks
-    return 0;
+    let count = 0;
+    todos.forEach((element) => {
+      if (element.checked == true) count++;
+    });
+    return count;
   };
 
-  return (
-    <div className="TodoResults">
-      Done: {calculateChecked()}
-    </div>
-  );
+  return <div className="TodoResults">Done: {calculateChecked()}</div>;
 };
